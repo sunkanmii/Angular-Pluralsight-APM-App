bf158a25de8e211becbf3a2dc1678f4f3b390a80
Fafowora Olasunkanmi <sunkanmifafowora@gmail.com>
2019-08-23 03:58:46 +0100

    Template for product list created

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  template: `
+    <div> 
+    <h1>{{pageTitle}}</h1>
+    <div>My First Component</div>
+    <div>My second Component</div>
+    </div>
+  `
+})
+
+export class AppComponent {
+  pageTitle = 'Acme Project Management';
+}
